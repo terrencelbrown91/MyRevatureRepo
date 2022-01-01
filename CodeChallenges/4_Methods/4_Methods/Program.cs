0814f303d5529d09305d69c32010ace1309e2934
Terrence <tmbrown9141@yahoo.com>
2021-12-31 23:15:18 -0500

    new commit

--- CodeChallenges/4_Methods/4_Methods/Program.cs
+++ CodeChallenges/4_Methods/4_Methods/Program.cs
@@ -10,30 +10,53 @@ namespace _4_MethodsChallenge
             /**
                 YOUR CODE HERE.
             **/
-            
-            
+
+
             
 
         }
 
-        public static string GetName(string name)
+        public static string GetName()
         {
-            // //throw new NotImplementedException("GetName() is not implemented yet0");
-            Console.WriteLine("What is your name");
-            string myRead = Console.ReadLine();
-
-            return myRead;
+            //throw new NotImplementedException("GetName() is not implemented yet0");
+            
+            Console.WriteLine("Please enter you name?");
+            
+            
+            return Console.ReadLine();
+            
         }
 
         public static string GreetFriend(string name)
         {
             //throw new NotImplementedException("GreetFriend() is not implemented yet");
+
+
+           return $"Hello, {name}.  You are my friend.";
+           
         }
 
         public static double GetNumber()
         {
-            throw new NotImplementedException("GetNumber() is not implemented yet");
+            //throw new NotImplementedException("GetNumber() is not implemented yet");
+            
+            double myDouble1;
+            double myDouble2;
+            string userInput1;
+            string userInput2;
+            double totalDouble =0;
+            do
+            {
+                userInput1 = Console.ReadLine();
+                userInput2 = Console.ReadLine();
 
+                if (double.TryParse(userInput1, out myDouble1) && double.TryParse(userInput2, out myDouble2))
+                {
+                   totalDouble = myDouble1 + myDouble2;
+                }
+            } while (!double.TryParse(userInput1, out myDouble1) && !double.TryParse(userInput2, out myDouble2));
+
+            return totalDouble;
         }
 
         public static int GetAction()
@@ -45,5 +68,7 @@ namespace _4_MethodsChallenge
         {
             throw new NotImplementedException("DoAction() is not implemented yet");
         }
+
+      
     }
 }
